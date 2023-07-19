3284f84fe9dc146e35a2e3ca789b5935caf6ab75
Erica Maranowski <128769940+emaranowski@users.noreply.github.com>
2023-07-18 19:24:22 -0700

    Initial commit

---
+++ src/PokeMoves.js
@@ -0,0 +1,24 @@
+import { moves } from "./data";
+import PokeMoveCard from "./PokeMoveCard";
+// import "/PokeMoveCard.css";
+
+function PokeMoves() {
+  return (
+    <div>
+      <h1>PokeMoves</h1>
+      <ul>
+        {moves.map((item) => (
+          <PokeMoveCard key={item.id} {...item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default PokeMoves;
+
+// {
+//   /* <li key={item.id}>
+// {item.id}. {item.move}
+// </li> */
+// }
